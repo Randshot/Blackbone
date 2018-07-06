9c562a40aebbf91ea7d6b5514f43c3660cee82c5
Randshot <randshot@norealm.xyz>
2018-07-06 18:32:42 +0200

    Add ascii version of GetErrorDescription

--- src/BlackBone/Misc/Utils.cpp
+++ src/BlackBone/Misc/Utils.cpp
@@ -193,6 +193,27 @@ std::wstring Utils::GetErrorDescription( NTSTATUS code )
 }
 
 /// <summary>
+/// Get system error description
+/// </summary>
+/// <param name="code">The code.</param>
+/// <returns>Error message</returns>
+std::string Utils::GetErrorDescriptionA(NTSTATUS code) {
+    LPSTR lpMsgBuf = nullptr;
+
+    if (FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_FROM_HMODULE |
+                           FORMAT_MESSAGE_IGNORE_INSERTS,
+                       GetModuleHandleA("ntdll.dll"), code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                       (LPSTR)&lpMsgBuf, 0, NULL) != 0) {
+        std::string ret(lpMsgBuf);
+
+        LocalFree(lpMsgBuf);
+        return ret;
+    }
+
+    return "";
+}
+
+/// <summary>
 /// Check if file exists
 /// </summary>
 /// <param name="path">Full-qualified file path</param>
